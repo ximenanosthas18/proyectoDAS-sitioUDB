f31cf1f6a9042736e3a337193fe7b3a66d3ec02c
ximenanosthas18 <ximenanosthas18@users.noreply.github.com>
2020-10-25 18:08:59 -0600

    subiendo proyecto a github

---
+++ PROYECTODAS/Models/CarreraClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PROYECTODAS.Models
+{
+    public class CarreraClass
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Campus { get; set; }
+        public string Tipo { get; set; }
+        public int Duracion { get; set; }
+    }
+}
---
+++ PROYECTODAS/Models/NoticiaClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PROYECTODAS.Models
+{
+    public class NoticiaClass
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Cuerpo { get; set; }
+        public DateTime Fecha { get; set; }
+    }
+}
